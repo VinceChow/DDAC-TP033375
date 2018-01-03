1c704020ab5c962350c4a4830c729c3b00a130c0
Vincent Chow <vincentchow96@gmail.com>
2018-01-03 23:43:41 +0800

    Install & configure Datetimepicker

--- DDAC-TP033375/App_Start/BundleConfig.cs
+++ DDAC-TP033375/App_Start/BundleConfig.cs
@@ -9,9 +9,12 @@ namespace DDAC_TP033375
 		public static void RegisterBundles(BundleCollection bundles)
 		{
 			bundles.Add(new ScriptBundle("~/bundles/lib").Include(
-					"~/Scripts/jquery-{version}.js",
-					"~/Scripts/bootstrap.js",
-					"~/Scripts/respond.js"));
+						"~/Scripts/jquery-{version}.js",
+						"~/Scripts/bootstrap.js",
+						"~/Scripts/respond.js",
+						// Bootstrap 3 DateTimePicker
+						"~/Scripts/moment.min.js",
+						"~/Scripts/bootstrap-datetimepicker.js"));
 
 			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
 						"~/Scripts/jquery.validate*"));
@@ -22,8 +25,9 @@ namespace DDAC_TP033375
 						"~/Scripts/modernizr-*"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
-					  "~/Content/bootstrap.css",
-					  "~/Content/site.css"));
+						"~/Content/bootstrap.css",
+						"~/Content/site.css",
+						"~/Content/bootstrap-datetimepicker.css"));
 		}
 	}
 }
