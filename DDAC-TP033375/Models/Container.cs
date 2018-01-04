d7b0e5b1c0f6f28775b9eb3730c46b180869eb31
Vincent Chow <vincentchow96@gmail.com>
2018-01-04 14:29:26 +0800

    Create Containers table

---
+++ DDAC-TP033375/Models/Container.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DDAC_TP033375.Models
+{
+	public class Container
+	{
+		public int Id { get; set; }
+
+		[Required]
+		[Display(Name = "Item Type")]
+		public string ItemType { get; set; }
+
+		[Required]
+		[Range(0, 30)]
+		[Display(Name = "Weight (tonne/t)")]
+		public Double WeightInTonne { get; set; }
+	}
+}
