e34c3dbbc78c69663db448b41afb9b5878e01296
Vincent Chow <vincentchow96@gmail.com>
2018-01-04 12:56:17 +0800

    Create RoleName model

---
+++ DDAC-TP033375/Models/RoleName.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DDAC_TP033375.Models
+{
+	public class RoleName
+	{
+		public const string Admin = "Admin";
+	}
+}
